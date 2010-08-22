a35d2317dd2e9438fd0331cb1b2b86a445fc202b
ivank <ivank@ludios.net>
2010-08-22 07:44:53 +0000

    regen-proto.sh: write the .pb.js file to js/, not browsernode/

--- regen-proto.sh
+++ regen-proto.sh
@@ -1,6 +1,7 @@
 #!/bin/zsh -e
 PROTOBUF="/opt/protobuf"
 protoc --plugin=$PROTOBUF/bin/protoc-gen-js "-I$PROTOBUF/include" -I. --js_out=. --python_out=. browsernode/**/*.proto
+mv browsernode/whiteboard_messages.pb.js ./js/whiteboard/
 
 # Remove unnecessary JavaScript-specific import that shouldn't show up anywhere.
 sed -i 's/^import js\.javascript_package_pb2//' browsernode/**/*_pb2.py
