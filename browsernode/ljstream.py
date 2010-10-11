4c38f1031bd5608a3d7b950aca7e87f812130a88
ivank <ivank@ludios.net>
2010-10-11 22:51:34 +0000

    browsernode/ljstream.py: actually parse the livejournal feed

--- browsernode/ljstream.py
+++ browsernode/ljstream.py
@@ -1,8 +1,16 @@
+import sys
+
+from BeautifulSoup import BeautifulStoneSoup
+
 from twisted.internet import endpoints, protocol
 from twisted.python import log
 
 
 class LjProtocol(protocol.Protocol):
+	"""
+	I parse the LiveJournal Atom stream.  Connect me to
+	C{atom.services.livejournal.com:80}.
+	"""
 	_START_FEED = "<feed "
 	_END_FEED = "</feed>"
 
@@ -19,8 +27,24 @@ Host: atom.services.livejournal.com\r
 
 
 	def feedStringReceived(self, feedString):
-		print feedString
+		try:
+			soup = BeautifulStoneSoup(feedString)
+		except Exception, e:
+			log.err(e, "Problem parsing %r" % (feedString,))
+		# We want the <title>, not the <title type="text"> (because that's just the journal title)
+		htmlTitle = soup.findAll(lambda tag: tag.name == u'title' and not tag.attrs)[0].string
 
+		# name, attrs looks like this:
+		# u'link', [(u'href', u'http://aksnbs.livejournal.com/15282.html')]
+		twoLinks = soup.findAll(lambda tag: tag.name == u'link' and len(tag.attrs) == 1 and tag.attrs[0][0] == u'href')
+		# The second link is the correct one.
+		postHref = twoLinks[1].attrs[0][1]
+
+		htmlContent = soup.findAll('content')[0].string
+
+		print 'title:', repr(htmlTitle)
+		print 'href:', repr(postHref)
+		#print repr(htmlContent)
 
 	def dataReceived(self, data):
 		self.buffer += data
@@ -52,6 +76,7 @@ class LjFactory(protocol.Factory):
 
 
 def main():
+	log.startLogging(sys.stdout)
 	from twisted.internet import reactor
 	ep = endpoints.TCP4ClientEndpoint(reactor, 'atom.services.livejournal.com', 80)
 	factory = LjFactory()
