308b593c35cd0cd14919c03633ef2a9bc087f04a
ivank <ivank@ludios.net>
2010-11-25 19:44:13 +0000

    Move css from static/ to css/ and use .scss (Sass); move Closure Library CSS to separate project; add targets_css.py for Closurizer

---
+++ targets_css.py
@@ -0,0 +1,13 @@
+# Note: input is relative to the sibling "css" directory.
+# output is relative to this file's directory.
+
+targets = [
+	{"input": "browsernode/whiteboard.scss",
+	"output": "browsernode/static/whiteboard.css"},
+
+	{"input": "browsernode/forum.scss",
+	"output": "browsernode/static/forum.css"},
+
+	{"input": "browsernode/ljstream.scss",
+	"output": "browsernode/static/ljstream.css"},
+]
