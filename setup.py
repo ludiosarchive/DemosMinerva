5052bf89ff9fc9afecc4937204c73f00d28a63b3
Ivan Kozik <ivan@ludios.org>
2011-06-12 04:40:34 +0000

    Add classifiers and author info to setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,19 @@ import demosminerva
 setup(
 	name='demosminerva',
 	version=demosminerva.__version__,
-	description="Demos that use Minerva, a Comet server and client",
+	description="Demos that show off Minerva, a Comet server and client",
+	url="https://github.com/ludios/DemosMinerva",
+	author="Ivan Kozik",
+	author_email="ivan@ludios.org",
+	classifiers=[
+		'Programming Language :: Python :: 2',
+		'Programming Language :: JavaScript',
+		'Development Status :: 3 - Alpha',
+		'Operating System :: OS Independent',
+		'Intended Audience :: Developers',
+		'Topic :: Internet :: WWW/HTTP',
+		'License :: OSI Approved :: Apache Software License',
+	],
 	packages=['demosminerva', 'twisted.plugins', 'js_demosminerva'],
 	package_data={
 		'demosminerva': ['*.html', 'static/*'],
